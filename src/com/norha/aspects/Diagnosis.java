78b7b8f6e0f9465391926a35c2852279fe4bd319
Robinclear <robin.peter.vkm@gmail.com>
2020-05-01 17:43:19 -0300

    this pointcut
    output:
    target Advice
    this Advice
    Pertol car Running

--- src/com/norha/aspects/Diagnosis.java
+++ src/com/norha/aspects/Diagnosis.java
@@ -4,23 +4,25 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-
 @Aspect
 public class Diagnosis {
 
-	 @Pointcut("target(com.norha.cars.Machine)") 
+	@Pointcut("target(com.norha.cars.Machine)")
 	public void targetPointcut() {
 	}
 
 	@Before("targetPointcut()")
 	public void targetAdvice() {
-		System.out.println("target Advice"); 
+		System.out.println("target Advice");
 	}
-	
-	/*
-	 * @Pointcut("execution (void run())") public void thisPointcut() { }
-	 * 
-	 * @Before("thisPointcut()") public void thisAdvice() {
-	 * System.out.println("this Advice"); }
-	 */
+
+	@Pointcut("this(com.norha.cars.Machine)")
+	public void thisPointcut() {
+	}
+
+	@Before("thisPointcut()")
+	public void thisAdvice() {
+		System.out.println("this Advice");
+	}
+
 }
