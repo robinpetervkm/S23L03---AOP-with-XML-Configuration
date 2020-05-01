39352928073b6956b3ef24f71d7e33e72f558675
Robinclear <robin.peter.vkm@gmail.com>
2020-05-01 12:44:34 -0300

    Around type
    Output:
    Around Advice Initial Msg
    Running
    Around Advice Later Msg

--- src/com/norha/aspects/Diagnosis.java
+++ src/com/norha/aspects/Diagnosis.java
@@ -1,39 +1,30 @@
 package com.norha.aspects;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Diagnosis {
 
-	/*
-	 * @Before("execution(void com.norha.cars.PertolCar.run())") public void
-	 * pertolEnginBefore() { System.out.println("Checking Pertol Engin"); }
-	 * 
-	 * @After("execution(void com.norha.cars.PerolCar.run())") public void
-	 * pertolEnginAfter() { System.out.println("Montering Pertol Engin"); }
-	 */
-
 	@Pointcut("execution(void com.norha.cars.ElectricCar.run())")
 	public void run() {}
 	
-	@Before("run()")
-	public void electricEnginBefore() {
-		System.out.println("Checking Electric Engin");
-	}
-
-	/*
-	 * @After("run()") public void electricEnginAfter() {
-	 * System.out.println("Montering Electric Engin"); }
-	 */
 	
-	@AfterThrowing("run()")
-	public void electricEnginAfter() {
-		System.out.println("Montering Electric Engin");
+	@Around("run()")
+	public void aroundAdvice(ProceedingJoinPoint jp) {
+		System.out.println("Around Advice Initial Msg");
+		
+		try {
+			jp.proceed();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Around Advice Later Msg");
 	}
 	
 	
