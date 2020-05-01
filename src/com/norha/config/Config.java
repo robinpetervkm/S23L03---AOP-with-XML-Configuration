abdf36c2c93eaeeb243534addccaf8f53b43958e
Robinclear <robin.peter.vkm@gmail.com>
2020-05-01 09:12:28 -0300

    Aspect Implemented

--- src/com/norha/config/Config.java
+++ src/com/norha/config/Config.java
@@ -2,12 +2,15 @@ package com.norha.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.norha.cars.ElectricCar;
+import com.norha.diagnosis.Diagnosis;
 import com.norha.service.Cars;
 import com.norha.service.PertolCar;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class Config {
 	
 	@Bean("electricCar")
@@ -23,5 +26,10 @@ public class Config {
 	@Bean("cars")
 	Cars cars(){
 		return new Cars();
+	}
+	
+	@Bean("diagnosis")
+	Diagnosis diagnosis() {
+		return new Diagnosis();
 	}
 }
--- src/com/norha/diagnosis/Diagnosis.java
+++ src/com/norha/diagnosis/Diagnosis.java
@@ -1,6 +1,11 @@
 package com.norha.diagnosis;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
 public class Diagnosis {
+	@Before("execution(void run())")
 	public void engin() {
 		System.out.println("Checking Engin");
 	}
